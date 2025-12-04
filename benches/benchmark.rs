bb6882e1f7a5fe4e97ee0bbe601eec2e4c719ee2
Konstantinos Stathopulos <19920164+stathopulos@users.noreply.github.com>
2025-12-03 20:58:44 -0700

    add benchmark

---
+++ benches/benchmark.rs
@@ -0,0 +1,26 @@
+#![feature(test)]
+extern crate test;
+
+macro_rules! benchmark {
+    ($($day:tt), *) => {
+        $(
+        mod $day {
+            use aoc2025::$day::{solve_part_1, solve_part_2};
+
+            use test::Bencher;
+
+            #[bench]
+            fn part1_bench(b: &mut Bencher) {
+                b.iter(|| solve_part_1());
+            }
+
+            #[bench]
+            fn part2_bench(b: &mut Bencher) {
+                b.iter(|| solve_part_2());
+            }
+        }
+    )*
+    };
+}
+
+benchmark!(day02, day03);
