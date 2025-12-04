1a9b796da71775a3aee545aa6aa81196533eb734
Konstantinos Stathopulos <19920164+stathopulos@users.noreply.github.com>
2025-12-03 21:03:19 -0700

    day02: rayon to speed up brute force solution

--- src/day02.rs
+++ src/day02.rs
@@ -1,31 +1,34 @@
+use rayon::prelude::*;
+
 const INPUT: &str = include_str!("../input/day02.txt");
 
-pub fn solve_part_1() -> i64 {
+pub fn solve_part_1() -> u64 {
     run_solution(INPUT, part_1)
 }
 
-pub fn solve_part_2() -> i64 {
+pub fn solve_part_2() -> u64 {
     run_solution(INPUT, part_2)
 }
 
-fn run_solution(input: &str, f: fn(i64) -> bool) -> i64 {
+fn run_solution(input: &str, f: fn(u64) -> bool) -> u64 {
     input
-        .split(",")
-        .map(|range_str| {
+        .par_split(',')
+        .flat_map(|range_str| {
             let (left, right) = range_str
                 .split_once("-")
                 .expect("Invalid input! Not a '-' seperated range!");
-            let range = left.parse::<i64>().expect("Left side of range not an int")
+            let range = left.parse::<u64>().expect("Left side of range not an int")
                 ..=right
                     .trim()
-                    .parse::<i64>()
+                    .parse::<u64>()
                     .expect("Right side of range not an int");
-            range.fold(0, |acc, num| if f(num) { acc + num } else { acc })
+            range
         })
+        .fold(|| 0, |acc, num| if f(num) { acc + num } else { acc })
         .sum()
 }
 
-fn digits(mut i: i64) -> Vec<u8> {
+fn digits(mut i: u64) -> Vec<u8> {
     let mut digits = Vec::with_capacity(19);
     while i > 0 {
         let n = (i % 10) as u8;
@@ -35,14 +38,14 @@ fn digits(mut i: i64) -> Vec<u8> {
     digits
 }
 
-fn part_1(i: i64) -> bool {
+fn part_1(i: u64) -> bool {
     let digits = digits(i);
 
     let (left, right) = digits.split_at(digits.len() / 2);
     *left == *right
 }
 
-fn part_2(i: i64) -> bool {
+fn part_2(i: u64) -> bool {
     let digits: Vec<u8> = digits(i);
     let len = digits.len();
 
