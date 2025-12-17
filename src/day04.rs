72da9aa6a9ba2a5f9148d7a549a072b1a9fb81b4
Konstantinos Stathopulos <19920164+stathopulos@users.noreply.github.com>
2025-12-17 00:21:46 -0700

    solve day04 part 2

--- src/day04.rs
+++ src/day04.rs
@@ -1,4 +1,4 @@
-use std::array;
+use std::{array, mem};
 
 #[cfg(feature = "input")]
 const INPUT: &str = include_str!("../input/day04.txt");
@@ -17,6 +17,11 @@ pub fn solve_part_1() -> usize {
     part_1::<INPUT_SIZE>(INPUT)
 }
 
+#[cfg(feature = "input")]
+pub fn solve_part_2() -> usize {
+    part_2::<INPUT_SIZE>(INPUT)
+}
+
 #[derive(Debug)]
 struct Grid<const N: usize>([[u8; N]; N]);
 
@@ -31,14 +36,27 @@ impl<const N: usize> Grid<N> {
         for maybe_i in [top, Some(idx_line), bottom] {
             for maybe_j in [left, Some(idx_byte), right] {
                 if let (Some(i), Some(j)) = (maybe_i, maybe_j)
-                        && self.0[i][j] == b'@'
-                    {
-                        count += 1;
+                    && self.0[i][j] == b'@'
+                {
+                    count += 1;
                 }
             }
         }
         count < 5
     }
+}
+
+fn remove_accessible<const N: usize>(grid: &mut Grid<N>) -> usize {
+    let mut count = 0;
+    for i in 0..N {
+        for j in 0..N {
+            if grid.0[i][j] == b'@' && grid.is_unblocked(i, j) {
+                count += 1;
+                let _ = mem::replace(&mut grid.0[i][j], b'.');
+            }
+        }
+    }
+    count
 }
 
 fn parse<const N: usize>(input: &str) -> Grid<N> {
@@ -57,6 +75,17 @@ fn part_1<const SIZE: usize>(input: &str) -> usize {
                 count += 1
             }
         }
+    }
+    count
+}
+
+fn part_2<const SIZE: usize>(input: &str) -> usize {
+    let mut grid: Grid<SIZE> = parse(input);
+    let mut count = 0;
+    let mut res = remove_accessible(&mut grid);
+    while res != 0 {
+        count += res;
+        res = remove_accessible(&mut grid);
     }
     count
 }
@@ -81,5 +110,12 @@ mod test {
         let answer = part_1::<DATA_SIZE>(DATA);
 
         assert_eq!(answer, 13)
+    }
+
+    #[test]
+    fn part_2_example() {
+        let answer = part_2::<DATA_SIZE>(DATA);
+
+        assert_eq!(answer, 43)
     }
 }
